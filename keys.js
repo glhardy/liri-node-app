fee8291d26b6e3c19f88e862d97672917521c855
Gabrielle Hardy <gabrhardy@gmail.com>
2017-08-15 18:33:40 -0500

    initializing

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('keys.js loading');
+
+exports.twitterKeys = {
+	consumer_key: 'g6TFgRmh4lU7jAKi7rEVZzMPC',
+	consumer_secret: '	w75l7KVLWJEvpObPOFTSCPYjJ4nghl4NH58aASBbf0kWdKXwK2',
+	access_token_key: '	781690524906483713-Oj8D6nXQYj9L8EGRm6IJIowU8V4i9Ff',
+	access_token_secret: '	sUelorRHG1GscxzbwcYZtL7dlD4D5ninfQCLpeWAaM857',
+}
+
+console.log('done loading twitter keys')
---
+++ liri.js
@@ -0,0 +1,3 @@
+var keys = require("./keys.js");
+
+var Twitter = require("twitter");
